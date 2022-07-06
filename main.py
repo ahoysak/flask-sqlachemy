4890d0d68ce6c9060dbd0c947f76202f3bbaf6e7
ahoysak <sinmirami@gmail.com>
2022-07-06 18:56:58 +0300

    Перехід з json на SQLalchemy

--- main.py
+++ main.py
@@ -1,223 +1,216 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-import json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqlaqwqer2@localhost/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    first_name = db.Column(db.String(15))
+    id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(15))
+    email = db.Column(db.String(15))
+    password = db.Column(db.String(15))
+    nickname = db.Column(db.String(20))
+    text = db.Column(db.String(20))
+
+class Cars(db.Model):
+    brand = db.Column(db.String(20))
+    type = db.Column(db.String(20))
+    number = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, primary_key=True)
+
 @app.route('/')
 def first_page():
     return render_template('firstpage.html')
-
+# Добавлення юзерів
 @app.route('/add', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        name = request.form.get('user_name')
-        text_msg = request.form.get('text_msg')
-        id = request.form.get('id')
-        email = request.form.get('email')
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        password = request.form.get('password')
-        print(name)
-        print(text_msg)
-        print(id)
-        print(email)
-        print(first_name)
-        print(last_name)
-        print(password)
-        with open('users.json') as f:
-            data = json.load(f)
+        nickname = request.form['nickname']
+        text = request.form['text']
+        id = request.form['id']
+        email = request.form['email']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
 
-        if name != '' and text_msg != '' and id != '' and email != '' and first_name != '' and last_name != '' and password != '':
-            name_text = {"nickname": name, "text": text_msg, "id": id, "email": email, "first_name": first_name, "last_name": last_name, "password": password}
-            print(name_text)
-            data.append(name_text)
-            with open('users.json', 'w') as f:
-                json.dump(data, f)
-
-        with open('users.json') as f:
-            data = json.load(f)
-
-        return render_template('index.html', data=data)
+        users = Users(nickname=nickname, text=text, id=id, email=email, first_name=first_name, last_name=last_name, password=password)
+        try:
+            db.session.add(users)
+            db.session.commit()
+            db.session.flush()
+            return redirect('/about')
+        except:
+            return 'Помилка! Дані не добавились!'
     else:
         return render_template('index.html')
-# всі команди
+# вивід даних про юзерів
 @app.route('/about', methods=['GET', 'POST'])
-def go_user():
-    with open('users.json') as f:
-        data = json.load(f)
-    return render_template('shows.html', data=data)
-# видалення
+def all_data_users():
+    all_data = Users.query.order_by(Users.id).all()
+    return render_template('/shows.html', all_data=all_data)
+
+
+# видалення користувача по ІД
 @app.route('/delete/<int:id>/del', methods=['GET', 'POST'])
-def delete_users(id):
+def delete_data_by_id(id):
+    delete_data = Users.query.get_or_404(id)
     try:
-        if request.method == 'GET':
-            file = open('users.json', 'r')
-            users = json.loads(file.read())
-            file.close()
-            counter = 0
-            for user in users:
-                if user['id'] == str(id):
-                    break
-                counter = counter + 1
-            del users[counter]
-            with open('users.json', 'w') as file:
-                file.write(json.dumps(users))
+        db.session.delete(delete_data)
+        db.session.commit()
         return redirect('/about')
     except:
-        return ' ne tak'
-# редагування
-@app.route('/update/<int:id>/up', methods=['GET'])
-def update_user_page(id):
-    file = open('users.json', 'r')
-    users = json.loads(file.read())
-    file.close()
-    for user in users:
-        if user['id'] == str(id):
-            return render_template('update.html', user=user)
+        return 'Помилка! Дані не видалились!'
 
-@app.route('/update/<int:id>/save', methods=['POST'])
-def save_user(id):
-    file = open('users.json', 'r')
-    users = json.loads(file.read())
-    file.close()
-    for user in users:
-        if user['id'] == str(id):
-            user['nickname'] = request.form.get('nickname')
-            user['text'] = request.form.get('text')
-            user['email'] = request.form.get('email')
-            user['first_name'] = request.form.get('first_name')
-            user['last_name'] = request.form.get('last_name')
-            user['password'] = request.form.get('password')
-    with open('users.json', 'w') as file:
-        file.write(json.dumps(users))
-    return redirect('/about')
-# ПОШУК ПО НІКУ
-@app.route('/search', methods=['GET'])
-def show_user():
-    file = open('users.json', 'r')
-    users = json.loads(file.read())
-    file.close()
-    nickname = request.args.get('nickname')
-    for user in users:
-        if user['nickname'] == nickname:
-            return render_template('/finded.html', searched=user)
-    return 'Error'
+# оновлення даних по ІД
+@app.route('/update/<int:id>/up', methods=['GET', 'POST'])
+def update_data_by_id(id):
+    users = Users.query.get(id)
+    if request.method == 'POST':
+        users.nickname = request.form['nickname']
+        users.text = request.form['text']
+        users.email = request.form['email']
+        users.first_name = request.form['first_name']
+        users.last_name = request.form['last_name']
+        users.password = request.form['password']
 
-@app.route('/find', methods=['GET'])
-def find():
-    return render_template('find.html')
-# ПЕРЕВІРКА ПО ЕМАЙЛУ І ПАРОЛЮ
-@app.route('/email', methods=['GET'])
-def show_command():
-    file = open('users.json', 'r')
-    users = json.loads(file.read())
-    file.close()
-    password = request.args.get('password')
-    email = request.args.get('email')
-    for user in users:
-        if user['password'] == password and user['email'] == email:
-            return render_template('/cheking.html')
-    return 'Error'
+        try:
+            db.session.commit()
+            return redirect('/about')
+        except:
+            return 'Помилка! Дані не оновились!'
+    else:
+        return render_template('update.html', update_data=users)
+# Оновленя машини по ІД
+@app.route('/update-car/<int:user_id>/up', methods=['GET', 'POST'])
+def update_cars_by_id(user_id):
+    cars = Cars.query.get(user_id)
+    if request.method == 'POST':
+        cars.brand = request.form['brand']
+        cars.type = request.form['type']
+        cars.number = request.form['number']
 
-@app.route('/check', methods=['GET'])
-def check_email():
-    return render_template('authorization.html')
+        try:
+            db.session.commit()
+            return redirect('/all-car')
+        except:
+            return 'Помилка'
+    else:
+        return render_template('updatecar.html', update_data_car=cars)
 
-
-# ВСІ МАШИНИ
-@app.route('/all-car', methods=['GET'])
+# Дані про машини
+@app.route('/all-car', methods=['GET', 'POST'])
 def all_auto():
-    with open('cars.json') as f:
-        data = json.load(f)
-    return render_template('car.html', data=data)
-# ДОДАВАННЯ АВТО
+    all_data = Cars.query.order_by(Cars.user_id).all()
+    return render_template('car.html', all_data=all_data)
+# Добавлення даних про машину
 @app.route('/add-car', methods=['GET', 'POST'])
 def add_new_car():
     if request.method == 'POST':
-        name = request.form.get('brand')
-        type_car = request.form.get('type')
-        number_car = request.form.get('number')
-        id_for_user = request.form.get('user_id')
-        print(name)
-        print(type_car)
-        print(number_car)
-        print(id_for_user)
-        with open('cars.json') as f:
-            data = json.load(f)
+        brand = request.form['brand']
+        type = request.form['type']
+        number = request.form['number']
+        user_id = request.form['user_id']
 
-        if name != '' and type_car != '' and number_car != '' and id_for_user != '':
-            write_data = {"brand": name, "type": type_car, "number": number_car, "user_id": id_for_user}
-            print(write_data)
-            data.append(write_data)
-            with open('cars.json', 'w') as f:
-                json.dump(data, f)
-
-        with open('cars.json') as f:
-            data = json.load(f)
-
-        return render_template('addcar.html', data=data)
+        cars = Cars(brand=brand, type=type, number=number, user_id=user_id)
+        try:
+            db.session.add(cars)
+            db.session.commit()
+            db.session.flush()
+            return redirect('/all-car')
+        except:
+            return 'Помилка! Дані не добавились!'
     else:
         return render_template('addcar.html')
-# ВИДАЛЕННЯ АВТО
+# Видалення машини по ІД
 @app.route('/delete-car/<int:user_id>/del', methods=['GET', 'POST'])
-def delete_car(user_id):
+def delete_car_by_id(user_id):
+    delete_data_car = Cars.query.get(user_id)
     try:
-        if request.method == 'GET':
-            file = open('cars.json', 'r')
-            users = json.loads(file.read())
-            file.close()
-            counter = 0
-            for user in users:
-                if user['user_id'] == str(user_id):
-                    break
-                counter = counter + 1
-            del users[counter]
-            with open('cars.json', 'w') as file:
-                file.write(json.dumps(users))
-
-        return redirect('/')
-    except:
-        return ' ne tak'
-#РЕДАГУВАННЯ АВТО
-@app.route('/update-car/<int:user_id>/up', methods=['GET'])
-def remove_cars(user_id):
-    file = open('cars.json', 'r')
-    users = json.loads(file.read())
-    file.close()
-    try:
-        for user in users:
-            if user['user_id'] == str(user_id):
-                return render_template('updatecar.html', user=user)
-    except:
-        return 'Error'
-
-@app.route('/update-car/<int:user_id>/save', methods=['POST'])
-def save_cars(user_id):
-    file = open('cars.json', 'r')
-    users = json.loads(file.read())
-    file.close()
-    for user in users:
-        if user['user_id'] == str(user_id):
-            user['brand'] = request.form.get('brand')
-            user['type'] = request.form.get('type')
-            user['number'] = request.form.get('number')
-    with open('cars.json', 'w') as file:
-        file.write(json.dumps(users))
+        db.session.delete(delete_data_car)
+        db.session.commit()
         return redirect('/all-car')
-# # ПОКАЗ АВТО
-@app.route('/same/<int:id>/id', methods=['POST', 'GET'])
-def check_id_auto_user(id):
-    file = open('cars.json', 'r')
-    cars = json.loads(file.read())
-    file.close()
-    user_cars = []
-    try:
-        for car in cars:
-            if str(id) == car['user_id']:
-                user_cars.append(car)
-        return render_template('sameid.html', cars=user_cars)
     except:
-        return 'Error'
+        return "Помилка!"
+
+
+# # ПОШУК ПО НІКУ
+# @app.route('/search', methods=['GET'])
+# def show_user():
+#     file = open('users.json', 'r')
+#     users = json.loads(file.read())
+#     file.close()
+#     nickname = request.args.get('nickname')
+#     for user in users:
+#         if user['nickname'] == nickname:
+#             return render_template('/finded.html', searched=user)
+#     return 'Error'
+#
+# @app.route('/find', methods=['GET'])
+# def find():
+#     return render_template('find.html')
+# # ПЕРЕВІРКА ПО ЕМАЙЛУ І ПАРОЛЮ
+# @app.route('/email', methods=['GET'])
+# def show_command():
+#     file = open('users.json', 'r')
+#     users = json.loads(file.read())
+#     file.close()
+#     password = request.args.get('password')
+#     email = request.args.get('email')
+#     for user in users:
+#         if user['password'] == password and user['email'] == email:
+#             return render_template('/cheking.html')
+#     return 'Error'
+#
+# @app.route('/check', methods=['GET'])
+# def check_email():
+#     return render_template('authorization.html')
+#
+#
+
+# #РЕДАГУВАННЯ АВТО
+# @app.route('/update-car/<int:user_id>/up', methods=['GET'])
+# def remove_cars(user_id):
+#     file = open('cars.json', 'r')
+#     users = json.loads(file.read())
+#     file.close()
+#     try:
+#         for user in users:
+#             if user['user_id'] == str(user_id):
+#                 return render_template('updatecar.html', user=user)
+#     except:
+#         return 'Error'
+#
+# @app.route('/update-car/<int:user_id>/save', methods=['POST'])
+# def save_cars(user_id):
+#     file = open('cars.json', 'r')
+#     users = json.loads(file.read())
+#     file.close()
+#     for user in users:
+#         if user['user_id'] == str(user_id):
+#             user['brand'] = request.form.get('brand')
+#             user['type'] = request.form.get('type')
+#             user['number'] = request.form.get('number')
+#     with open('cars.json', 'w') as file:
+#         file.write(json.dumps(users))
+#         return redirect('/all-car')
+# # # ПОКАЗ АВТО
+# @app.route('/same/<int:id>/id', methods=['POST', 'GET'])
+# def check_id_auto_user(id):
+#     file = open('cars.json', 'r')
+#     cars = json.loads(file.read())
+#     file.close()
+#     user_cars = []
+#     try:
+#         for car in cars:
+#             if str(id) == car['user_id']:
+#                 user_cars.append(car)
+#         return render_template('sameid.html', cars=user_cars)
+#     except:
+#         return 'Error'
 
 
 
